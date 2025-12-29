6d4fa24516a5cd71a18e3b1772d080ab0ff745ad
niajaya875-creator <niajaya875@gmail.com>
2025-12-29 18:03:25 +0700

    Add files via upload

---
+++ aastabaya/asgi.py
@@ -0,0 +1,38 @@
+"""
+ASGI config for aastabaya project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+from django.conf import settings
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.contrib.staticfiles.handlers import ASGIStaticFilesHandler
+import apps.routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aastabaya.settings')
+
+# Initialize Django ASGI application early to ensure AppRegistry is populated
+# before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            apps.routing.websocket_urlpatterns
+        )
+    ),
+}) if not settings.DEBUG else ProtocolTypeRouter({
+    "http": ASGIStaticFilesHandler(django_asgi_app),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            apps.routing.websocket_urlpatterns
+        )
+    ),
+})
